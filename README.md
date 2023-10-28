563e00828b4ef027a74e70043cf2a7893769d176
Bishoy Labib <132815670+BishoyLabib7@users.noreply.github.com>
2023-10-28 19:04:15 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# Guess My Number Game
+
+Guess my number is a simple game in which the user:
+
+- Enter guess (from 1-20)
+- See if your guess is too high or too low
+- Change guess based on programm instructions (if guess to low choose a number greater than current and vice versa)
+- Check number of guesses
+- Check highest guess score
+
+<img src="https://github.com/somekindofwallflower/guess-my-number/blob/master/guess_my_number.gif" />
+
+## Live Demo
+
+**You Can See The Project Live On:** [Here]()
+
+## Description
+
+A Simple Guess My Number Game Using Javascript.
+The Player Should Guess A Number Between `(1 - 20)`
+You Have 20 Chances To Guess The Number Correct.
+
+Technologies:
+
+- HTML
+- CSS
+- JS
---
+++ script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+let number = Math.trunc(Math.random() * 20) + 1;
+console.log(number);
+let wins = true;
+
+const displayMessage = function (place, message) {
+  document.querySelector(place).textContent = message;
+};
+//again button
+document.querySelector(".again").addEventListener("click", function () {
+  wins = true;
+  displayMessage(".number", "?");
+  displayMessage(".message", "Start guessing...");
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  displayMessage(".score", 20);
+  number = Math.trunc(Math.random() * 20) + 1;
+  console.log(number);
+});
+
+//check button
+document.querySelector(".check").addEventListener("click", function () {
+  if (wins) {
+    const usernumber = Number(document.querySelector(".guess").value);
+    let message;
+    let highscore = Number(document.querySelector(".highscore").textContent);
+    let score = Number(document.querySelector(".score").textContent);
+
+    // When usernumber is not input
+    if (!usernumber) message = "⛔ No number !";
+    //When player is wins
+    else if (usernumber === number) {
+      message = "🎉 Correct Number!";
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      document.querySelector(".number").style.width = "30rem";
+      wins = false;
+      displayMessage(".number", number);
+      if (score > highscore) {
+        highscore = score;
+        displayMessage(".highscore", highscore);
+      }
+
+      // When guess is wrong
+    } else {
+      if (score > 1) {
+        message = usernumber < number ? "📈 TOO low!" : "📈 TOO high!";
+        score--;
+        displayMessage(".score", score);
+      } else {
+        message = "💥 You Lost the game!";
+        document.querySelector("body").style.backgroundColor = "#900a0a";
+        wins = false;
+        displayMessage(".score", 0);
+      }
+    }
+    displayMessage(".message", message);
+  }
+});
